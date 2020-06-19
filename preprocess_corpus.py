30993592ac222fd81593d82c3c968f191aedea06
fcpi <pifuchan@yeah.net>
2020-06-19 10:06:32 +0800

    Update preprocess_corpus.py

--- preprocess_corpus.py
+++ preprocess_corpus.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
-
+import os
 import glob
 import json
 
-
-base_dir = "/path_to_/txt/wiki_zh_2019/*/*"
+base_dir = "/path_to_/wiki_zh_2019"
+for sub_folder in os.listdir(base_dir):
+    sub_folder_path = os.path.join(base_dir, sub_folder)
+    for file in os.listdir(sub_folder_path):
+        cur_file = os.path.join(sub_folder_path, file)
+        if os.path.exists(cur_file):
+            cur_file_saved = sub_folder_path + "_" + file + ".json"
+            os.system("mv %s %s" % (cur_file, cur_file_saved))
+            
+            
+base_dir = "/path_to_txt/*/*"
 file_paths = sorted(glob.glob(base_dir))
 for input_path in file_paths:
     print("Processing %s", input_path)
